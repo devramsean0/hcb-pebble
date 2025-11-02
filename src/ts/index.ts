24a4d5212494097411fcf234e07cbbe7686fc2b4
Sean Outram <me@sean.cyou>
2025-11-02 12:27:04 +0000

    Continue with project scaffold

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,1 +1,25 @@
-Pebble.addEventListener("ready", async (e) => console.log("Hello from TS!"))
+Pebble.addEventListener("ready", (e) => {
+    PebbleTS.sendAppMessage({'APP_READY': true });
+});
+
+Pebble.addEventListener("appmessage", (dict) => {
+    switch (dict.payload["REQUEST"]) {
+        case "LIST_ORG":
+            
+    }
+});
+
+
+function xhrWrapper(url: string, type: string, data: any, callback: (xhr: XMLHttpRequest) => {}) {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    callback(xhr);
+  };
+  xhr.open(type, url);
+  if(data) {
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify(data));
+  } else {
+    xhr.send();
+  }
+}
